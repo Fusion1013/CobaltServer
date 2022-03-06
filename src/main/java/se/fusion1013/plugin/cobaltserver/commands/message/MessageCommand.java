5f51bb20220671c1f000f36604b5815e6a986d24
Fusion1013 <william.reutmer@gmail.com>
2022-03-06 17:18:26 +0100

    Create MessageCommand.java

---
+++ src/main/java/se/fusion1013/plugin/cobaltserver/commands/message/MessageCommand.java
@@ -0,0 +1,63 @@
+package se.fusion1013.plugin.cobaltserver.commands.message;
+
+import dev.jorel.commandapi.CommandAPI;
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.EntitySelectorArgument;
+import dev.jorel.commandapi.arguments.GreedyStringArgument;
+import dev.jorel.commandapi.arguments.PlayerArgument;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import se.fusion1013.plugin.cobaltcore.manager.LocaleManager;
+import se.fusion1013.plugin.cobaltcore.util.PlayerUtil;
+import se.fusion1013.plugin.cobaltcore.util.StringPlaceholders;
+
+import java.util.Collection;
+
+public class MessageCommand {
+
+    // ----- REGISTER -----
+
+    public static void register() {
+        CommandAPI.unregister("msg", true);
+        CommandAPI.unregister("tell", true);
+        CommandAPI.unregister("w", true);
+
+        new CommandAPICommand("msg")
+                .withAliases("tell", "w")
+                .withArguments(new EntitySelectorArgument("targets", EntitySelectorArgument.EntitySelector.MANY_ENTITIES).replaceSuggestions(PlayerUtil::getPlayerArguments))
+                .withArguments(new GreedyStringArgument("message"))
+                .executes(((sender, args) -> {
+                    Collection<Entity> targets = (Collection<Entity>) args[0];
+                    String message = (String) args[1];
+
+                    for (Entity target : targets) {
+                        // Create placeholder that contains entity info and message info
+                        StringPlaceholders placeholders = StringPlaceholders.builder()
+                                .addPlaceholder("sender", sender.getName())
+                                .addPlaceholder("receiver", target.getName())
+                                .addPlaceholder("message", message)
+                                .build();
+
+                        // Check if sender is a player
+                        if (sender instanceof Player player) {
+
+                            if (target instanceof Player targetPlayer) {
+
+                                // If player is vanished and sender doesn't have permission to vanish, don't send the message and instead send an error message to the sender
+                                if (PlayerUtil.isVanished(targetPlayer) && !sender.hasPermission("cobalt.server.commands.vanish")) {
+                                    LocaleManager.getInstance().sendMessage("", player, "commands.error.player_does_not_exist");
+                                } else {
+                                    // Send success message to both the sender and the target
+                                    LocaleManager.getInstance().sendMessage("", player, "commands.message.sender", placeholders);
+                                    LocaleManager.getInstance().sendMessage("", targetPlayer, "commands.message.receiver", placeholders);
+                                }
+                            } else {
+                                LocaleManager.getInstance().sendMessage("", player, "commands.message.sender", placeholders);
+                            }
+                        }
+                    }
+                })).register();
+    }
+
+}
